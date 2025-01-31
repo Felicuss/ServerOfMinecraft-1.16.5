bd697b49a826314a0f9979687c664b736b83d211
Felicuss <m2409429@edu.misis.ru>
2025-01-31 13:53:47 +0000

    Pending changes exported from your codespace

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while [ true ]; do
+    java -Xms1G -Xmx12G --add-modules=jdk.incubator.vector -XX:+UseG1GC -DPaper.IgnoreJavaVersion=true -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1  -Daikars.new.flags=true -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -jar purpur.jar --nogui
+
+    echo Server restarting...
+    echo Press CTRL + C to stop.
+done
